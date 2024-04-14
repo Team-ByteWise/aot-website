c6eb98785a7c08881f813fde6be064747f2ece3a
Ankan Pal <itsyourap@gmail.com>
2024-04-14 10:19:31 +0530

    Define default Home Page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
 function App() {
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
