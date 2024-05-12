1d457796f09dd96a6a32945302de62167091849c
Ankan Pal <itsyourap@gmail.com>
2024-05-12 10:19:15 +0530

    Implement Carousel Component

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,176 @@
+import React, { CSSProperties } from "react";
+import Slider, { Settings } from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Typography, Button, styled } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+interface Slide {
+  image: string;
+  alt: string;
+  text: string;
+  buttonText: string;
+}
+
+const ElevatedButton = styled(Button)({
+  backgroundColor: "rgba(255, 255, 255, 0.3)",
+  borderRadius: "8px",
+  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+  "&:hover": {
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
+  },
+});
+
+const NextArrow: React.FC<{ onClick?: () => void }> = ({ onClick }) => (
+  <ElevatedButton
+    onClick={onClick}
+    style={{
+      position: "absolute",
+      right: "10px",
+      top: "50%",
+      transform: "translateY(-100%)",
+      zIndex: 1,
+    }}
+  >
+    <ArrowForwardIcon />
+  </ElevatedButton>
+);
+
+const PrevArrow: React.FC<{ onClick?: () => void }> = ({ onClick }) => (
+  <ElevatedButton
+    onClick={onClick}
+    style={{
+      position: "absolute",
+      left: "10px",
+      top: "50%",
+      transform: "translateY(-100%)",
+      zIndex: 1,
+    }}
+  >
+    <ArrowBackIcon />
+  </ElevatedButton>
+);
+
+const Carousel = ({ slides, sx }: { slides: Slide[]; sx: CSSProperties }) => {
+  const settings: Settings = {
+    dots: true,
+    appendDots: (dots) => (
+      <div
+        style={{
+          position: "absolute",
+          zIndex: 5,
+          bottom: "5%",
+          backgroundColor: "rgba(255, 255, 255, 0.4)",
+          borderRadius: "10px",
+          padding: "10px",
+          width: "max-content",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <ul
+          style={{
+            margin: "0px",
+            padding: "0px",
+            alignItems: "center",
+            display: "flex",
+          }}
+        >
+          {dots}
+        </ul>
+      </div>
+    ),
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  const floatingStyle: React.CSSProperties = {
+    position: "absolute",
+    left: "5%",
+    zIndex: 2,
+    textAlign: "left",
+  };
+
+  const imageProperties: React.CSSProperties = {
+    width: "100%",
+    objectFit: "cover",
+    height: sx.height,
+    borderRadius: sx.borderRadius,
+  };
+
+  return (
+    <div
+      style={{ height: sx.height, position: "relative", overflow: "hidden" }}
+    >
+      <Slider {...settings}>
+        {slides.map((slide, index) => (
+          <div key={index}>
+            <div
+              style={{
+                overflow: "hidden",
+                borderRadius: sx.borderRadius,
+                height: sx.height,
+              }}
+            >
+              <img src={slide.image} alt={slide.alt} style={imageProperties} />
+              <div
+                style={{
+                  position: "absolute",
+                  top: "0",
+                  left: "0",
+                  width: "100%",
+                  height: sx.height,
+                  backgroundColor: "rgba(0, 0, 0, 0.3)",
+                  borderRadius: sx.borderRadius,
+                }}
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  top: "0",
+                  left: "0",
+                  width: "100%",
+                  height: sx.height,
+                  backgroundImage:
+                    "linear-gradient(to right, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0))",
+                  borderRadius: sx.borderRadius,
+                }}
+              />
+            </div>
+
+            <Typography
+              style={{ ...floatingStyle, top: "20%" }}
+              color={"primary.dark"}
+              fontWeight={"bold"}
+              fontSize={"2rem"}
+              maxWidth={"30%"}
+              variant="h5"
+              component="h2"
+            >
+              {slide.text}
+            </Typography>
+
+            <Button
+              style={{ ...floatingStyle, bottom: "30%" }}
+              variant="contained"
+              color="primary"
+              sx={{ p: 3 }}
+            >
+              {slide.buttonText}
+            </Button>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
