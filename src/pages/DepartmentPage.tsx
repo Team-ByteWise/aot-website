98364ebd029c6c957868ded90f5a2bc45a094bb9
Ankan Pal <itsyourap@gmail.com>
2024-06-02 00:48:42 +0530

    Create local horizontal nav bar on department page

--- src/pages/DepartmentPage.tsx
+++ src/pages/DepartmentPage.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  AppBar,
+  Box,
+  Button,
   Container,
   PaletteMode,
   Stack,
+  Toolbar,
   Typography,
   useMediaQuery,
 } from "@mui/material";
@@ -19,9 +23,13 @@ import CustomCard from "../components/CustomCard/CustomCard";
 import person from "../assets/images/person.png";
 import NavBarItems from "../data/NavBarItems";
 
+const subNavBarItems = ["About", "Faculty", "Events"];
+
 export default function DepartmentPage() {
   let currentTheme = localStorage.getItem("theme");
   if (!currentTheme) currentTheme = "light";
+
+  const [selectedButton, setSelectedButton] = useState<string>();
 
   const [mode, setMode] = React.useState<PaletteMode>(
     currentTheme as "dark" | "light"
@@ -35,6 +43,10 @@ export default function DepartmentPage() {
   useEffect(() => {
     localStorage.setItem("theme", mode);
   }, [mode]);
+
+  useEffect(() => {
+    setSelectedButton(subNavBarItems[0]);
+  }, []);
 
   const navigate = useNavigate();
   const matchesSM = useMediaQuery(Theme.breakpoints.down("sm"));
@@ -61,225 +73,272 @@ export default function DepartmentPage() {
           mt: { xs: 15, md: 25 },
         }}
       >
-        <Stack
-          spacing={{ xs: 2, md: 5, lg: 10 }}
-          direction={{ xs: "column", md: "column", lg: "row" }}
-          sx={{ pb: { xs: 5, md: 5, lg: 10 } }}
+        <AppBar
+          position="static"
+          sx={{
+            boxShadow: 0,
+            bgcolor: "transparent",
+            backgroundImage: "none",
+          }}
         >
-          <img
-            src={image5}
-            alt={"Alt"}
-            style={{
-              width: "auto",
-              objectFit: "cover",
-              height: matchesSM ? "150px" : matchesMD ? "250px" : "350px",
-              boxShadow: "0 0 12px 5px #00000029",
+          <Toolbar
+            sx={{
+              borderBottom: "1px solid rgba(247, 248, 250, 1)",
+              mb: 5,
             }}
-          />
-          <Stack spacing={{ xs: 2, md: 5, lg: 8 }} direction={"column"}>
-            <Typography
-              textAlign={{ xs: "center", md: "center", lg: "left" }}
-              variant="h4"
-              fontWeight={"bold"}
-              marginBottom={"20px"}
-              sx={{
-                fontSize: { xs: 25, sm: 40 },
-                "& span": { color: "#1F278D" },
-              }}
+          >
+            <Stack
+              direction={"row"}
+              spacing={7}
+              justifyContent={"center"}
+              alignItems={"center"}
+              sx={{ width: "100%" }}
             >
-              Welcome to AOT, Department of{"\n"}
-              <span>{departments[dept].name}</span>
-            </Typography>
-            <Typography
-              textAlign={{ xs: "center", md: "center", lg: "left" }}
-              variant="h5"
-              fontWeight={"bold"}
-              marginBottom={"20px"}
-              sx={{ fontSize: 20 }}
-            >
-              “{departments[dept].tagLine}”
-            </Typography>
-          </Stack>
-        </Stack>
+              {subNavBarItems.map((item) => (
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    setSelectedButton(item);
+                  }}
+                  sx={{
+                    backgroundColor:
+                      selectedButton === item ? "primary.main" : "inherit", // Highlight the selected button
+                    color: selectedButton === item ? "#fff" : "",
+                  }}
+                >
+                  {item}
+                </Button>
+              ))}
+            </Stack>
+          </Toolbar>
+        </AppBar>
 
-        <Stack
-          spacing={{ xs: 2, md: 5, lg: 10 }}
-          direction={{ xs: "column", md: "column", lg: "row" }}
-          sx={{ pb: { xs: 5, md: 7, lg: 10 }, pt: { xs: 5, md: 7, lg: 10 } }}
-        >
-          <Stack spacing={{ xs: 2, md: 5, lg: 8 }} direction={"column"}>
-            <Typography
-              textAlign={"center"}
-              variant="h4"
-              fontWeight={"bold"}
-              marginBottom={"20px"}
-              sx={{
-                fontSize: { xs: 25, sm: 40 },
-                "& span": { color: "#1F278D" },
+        <Box sx={{}}>
+          <Stack
+            spacing={{ xs: 2, md: 5, lg: 10 }}
+            direction={{ xs: "column", md: "column", lg: "row" }}
+            sx={{ pb: { xs: 5, md: 5, lg: 10 } }}
+          >
+            <img
+              src={image5}
+              alt={"Alt"}
+              style={{
+                width: "auto",
+                objectFit: "cover",
+                height: matchesSM ? "150px" : matchesMD ? "250px" : "350px",
+                boxShadow: "0 0 12px 5px #00000029",
               }}
-            >
-              About our <span>Department</span>
-            </Typography>
-            <Typography
-              variant="h5"
-              textAlign={"center"}
-              fontWeight={"regular"}
-              marginBottom={"20px"}
-              sx={{ fontSize: { xs: 15, sm: 18, md: 20 } }}
-            >
-              {departments[dept].about}
-            </Typography>
-          </Stack>
-          <img
-            src={image5}
-            alt={"Alt"}
-            style={{
-              width: "auto",
-              objectFit: "cover",
-              height: matchesSM ? "150px" : matchesMD ? "250px" : "350px",
-              boxShadow: "0 0 12px 5px #00000029",
-            }}
-          />
-        </Stack>
-
-        <Stack
-          spacing={{ xs: 2, md: 5, lg: 10 }}
-          direction={{ xs: "column", md: "column", lg: "row" }}
-          sx={{ pb: { xs: 5, md: 7, lg: 10 }, pt: { xs: 5, md: 7, lg: 10 } }}
-        >
-          <img
-            src={image5}
-            alt={"Alt"}
-            style={{
-              width: "auto",
-              objectFit: "cover",
-              height: matchesSM ? "150px" : matchesMD ? "250px" : "350px",
-              boxShadow: "0 0 12px 5px #00000029",
-            }}
-          />
-          <Stack spacing={{ xs: 2, md: 5, lg: 8 }} direction={"column"}>
-            <Typography
-              textAlign={"center"}
-              variant="h4"
-              fontWeight={"bold"}
-              marginBottom={"20px"}
-              sx={{
-                fontSize: { xs: 25, sm: 40 },
-                "& span": { color: "#1F278D" },
-              }}
-            >
-              {departments[dept].placementStatistics.year}
-              {" Placement "}
-              <span>Statistics</span>
-            </Typography>
-            <Stack spacing={-0.05} direction={"column"}>
+            />
+            <Stack spacing={{ xs: 2, md: 5, lg: 8 }} direction={"column"}>
               <Typography
-                variant="h5"
-                textAlign={"center"}
+                textAlign={{ xs: "center", md: "center", lg: "left" }}
+                variant="h4"
                 fontWeight={"bold"}
                 marginBottom={"20px"}
                 sx={{
-                  fontSize: { xs: 15, sm: 18, md: 20 },
+                  fontSize: { xs: 25, sm: 40 },
                   "& span": { color: "#1F278D" },
                 }}
               >
-                <span>{departments[dept].placementStatistics.percentage}%</span>{" "}
-                Overall Placement
+                Welcome to AOT, Department of{"\n"}
+                <span>{departments[dept].name}</span>
               </Typography>
               <Typography
+                textAlign={{ xs: "center", md: "center", lg: "left" }}
                 variant="h5"
-                textAlign={"center"}
                 fontWeight={"bold"}
                 marginBottom={"20px"}
-                sx={{
-                  fontSize: { xs: 15, sm: 18, md: 20 },
-                  "& span": { color: "#1F278D" },
-                }}
+                sx={{ fontSize: 20 }}
               >
-                (
-                <span>
-                  {departments[dept].placementStatistics.placedStudents}
-                </span>{" "}
-                Students got{" "}
-                <span>{departments[dept].placementStatistics.totalOffers}</span>{" "}
-                Placement Offers)
+                “{departments[dept].tagLine}”
               </Typography>
             </Stack>
           </Stack>
-        </Stack>
 
-        <Container
-          maxWidth="xl"
-          sx={{ pb: { xs: 5, md: 7, lg: 10 }, pt: { xs: 5, md: 7, lg: 10 } }}
-        >
-          <Stack spacing={{ xs: 1, md: 2, lg: 3 }} direction={"column"}>
-            <Typography
-              textAlign={"center"}
-              variant="h4"
-              fontWeight={"bold"}
-              marginBottom={"20px"}
-              sx={{
-                fontSize: { xs: 25, sm: 40 },
-                "& span": { color: "#1F278D" },
+          <Stack
+            spacing={{ xs: 2, md: 5, lg: 10 }}
+            direction={{ xs: "column", md: "column", lg: "row" }}
+            sx={{ pb: { xs: 5, md: 7, lg: 10 }, pt: { xs: 5, md: 7, lg: 10 } }}
+          >
+            <Stack spacing={{ xs: 2, md: 5, lg: 8 }} direction={"column"}>
+              <Typography
+                textAlign={"center"}
+                variant="h4"
+                fontWeight={"bold"}
+                marginBottom={"20px"}
+                sx={{
+                  fontSize: { xs: 25, sm: 40 },
+                  "& span": { color: "#1F278D" },
+                }}
+              >
+                About our <span>Department</span>
+              </Typography>
+              <Typography
+                variant="h5"
+                textAlign={"center"}
+                fontWeight={"regular"}
+                marginBottom={"20px"}
+                sx={{ fontSize: { xs: 15, sm: 18, md: 20 } }}
+              >
+                {departments[dept].about}
+              </Typography>
+            </Stack>
+            <img
+              src={image5}
+              alt={"Alt"}
+              style={{
+                width: "auto",
+                objectFit: "cover",
+                height: matchesSM ? "150px" : matchesMD ? "250px" : "350px",
+                boxShadow: "0 0 12px 5px #00000029",
               }}
-            >
-              HOD's <span>Message</span>
-            </Typography>
-            <CustomCard>
-              <Stack spacing={{ xs: 1, md: 2, lg: 3 }} direction={"column"}>
-                <Stack direction={"row"} spacing={2} alignItems="center">
-                  <img
-                    src={person}
-                    alt="SS"
-                    style={{
-                      borderRadius: "50%",
-                      objectFit: "cover",
-                      width: "50px",
-                      height: "50px",
-                    }}
-                  />
-                  <Stack spacing={-0.5} direction={"column"}>
-                    <Typography
-                      variant="h5"
-                      fontWeight={"bold"}
-                      marginBottom={"20px"}
-                      sx={{
-                        fontSize: { xs: 15, sm: 20 },
-                        "& span": { color: "#1F278D" },
-                      }}
-                    >
-                      Mr. AOT
-                    </Typography>
+            />
+          </Stack>
 
-                    <Typography
-                      variant="subtitle1"
-                      marginBottom={"20px"}
-                      sx={{
-                        fontSize: { xs: 10, sm: 15 },
-                        "& span": { color: "#1F278D" },
-                      }}
-                    >
-                      Head of Department
-                    </Typography>
-                  </Stack>
-                </Stack>
-
+          <Stack
+            spacing={{ xs: 2, md: 5, lg: 10 }}
+            direction={{ xs: "column", md: "column", lg: "row" }}
+            sx={{ pb: { xs: 5, md: 7, lg: 10 }, pt: { xs: 5, md: 7, lg: 10 } }}
+          >
+            <img
+              src={image5}
+              alt={"Alt"}
+              style={{
+                width: "auto",
+                objectFit: "cover",
+                height: matchesSM ? "150px" : matchesMD ? "250px" : "350px",
+                boxShadow: "0 0 12px 5px #00000029",
+              }}
+            />
+            <Stack spacing={{ xs: 2, md: 5, lg: 8 }} direction={"column"}>
+              <Typography
+                textAlign={"center"}
+                variant="h4"
+                fontWeight={"bold"}
+                marginBottom={"20px"}
+                sx={{
+                  fontSize: { xs: 25, sm: 40 },
+                  "& span": { color: "#1F278D" },
+                }}
+              >
+                {departments[dept].placementStatistics.year}
+                {" Placement "}
+                <span>Statistics</span>
+              </Typography>
+              <Stack spacing={-0.05} direction={"column"}>
                 <Typography
+                  variant="h5"
                   textAlign={"center"}
-                  variant="body1"
+                  fontWeight={"bold"}
                   marginBottom={"20px"}
                   sx={{
-                    fontSize: { xs: 15, sm: 20 },
+                    fontSize: { xs: 15, sm: 18, md: 20 },
                     "& span": { color: "#1F278D" },
                   }}
                 >
-                  “{departments[dept].about}”
+                  <span>
+                    {departments[dept].placementStatistics.percentage}%
+                  </span>{" "}
+                  Overall Placement
+                </Typography>
+                <Typography
+                  variant="h5"
+                  textAlign={"center"}
+                  fontWeight={"bold"}
+                  marginBottom={"20px"}
+                  sx={{
+                    fontSize: { xs: 15, sm: 18, md: 20 },
+                    "& span": { color: "#1F278D" },
+                  }}
+                >
+                  (
+                  <span>
+                    {departments[dept].placementStatistics.placedStudents}
+                  </span>{" "}
+                  Students got{" "}
+                  <span>
+                    {departments[dept].placementStatistics.totalOffers}
+                  </span>{" "}
+                  Placement Offers)
                 </Typography>
               </Stack>
-            </CustomCard>
+            </Stack>
           </Stack>
-        </Container>
+
+          <Container
+            maxWidth="xl"
+            sx={{ pb: { xs: 5, md: 7, lg: 10 }, pt: { xs: 5, md: 7, lg: 10 } }}
+          >
+            <Stack spacing={{ xs: 1, md: 2, lg: 3 }} direction={"column"}>
+              <Typography
+                textAlign={"center"}
+                variant="h4"
+                fontWeight={"bold"}
+                marginBottom={"20px"}
+                sx={{
+                  fontSize: { xs: 25, sm: 40 },
+                  "& span": { color: "#1F278D" },
+                }}
+              >
+                HOD's <span>Message</span>
+              </Typography>
+              <CustomCard>
+                <Stack spacing={{ xs: 1, md: 2, lg: 3 }} direction={"column"}>
+                  <Stack direction={"row"} spacing={2} alignItems="center">
+                    <img
+                      src={person}
+                      alt="SS"
+                      style={{
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                        width: "50px",
+                        height: "50px",
+                      }}
+                    />
+                    <Stack spacing={-0.5} direction={"column"}>
+                      <Typography
+                        variant="h5"
+                        fontWeight={"bold"}
+                        marginBottom={"20px"}
+                        sx={{
+                          fontSize: { xs: 15, sm: 20 },
+                          "& span": { color: "#1F278D" },
+                        }}
+                      >
+                        Mr. AOT
+                      </Typography>
+
+                      <Typography
+                        variant="subtitle1"
+                        marginBottom={"20px"}
+                        sx={{
+                          fontSize: { xs: 10, sm: 15 },
+                          "& span": { color: "#1F278D" },
+                        }}
+                      >
+                        Head of Department
+                      </Typography>
+                    </Stack>
+                  </Stack>
+
+                  <Typography
+                    textAlign={"center"}
+                    variant="body1"
+                    marginBottom={"20px"}
+                    sx={{
+                      fontSize: { xs: 15, sm: 20 },
+                      "& span": { color: "#1F278D" },
+                    }}
+                  >
+                    “{departments[dept].about}”
+                  </Typography>
+                </Stack>
+              </CustomCard>
+            </Stack>
+          </Container>
+        </Box>
       </Container>
+
       <Footer mode={mode} />
     </ThemeProvider>
   );
