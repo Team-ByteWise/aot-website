d6555e81ef30779b04cf2b53a2a86a9e11449af1
Ankan Pal <itsyourap@gmail.com>
2024-06-03 13:51:38 +0530

    Randomize Event Data

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Event } from "../../data/Event";
 import CustomCard from "../CustomCard/CustomCard";
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
@@ -7,6 +8,22 @@ interface EventCardProps {
 }
 
 const EventCard = ({ event }: EventCardProps) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  const handleImageError = () => {
+    // Handle image loading error if needed
+    setImageLoaded(false);
+  };
+
+  useEffect(() => {
+    // Fix: React reusing component and displaying previously rendered event's image
+    setImageLoaded(false);
+  }, [event]);
+
   return (
     <>
       <CustomCard>
@@ -23,20 +40,28 @@ const EventCard = ({ event }: EventCardProps) => {
             image={event.image}
             alt="Placeholder Image"
             sx={{
-              width: "50%",
+              width: imageLoaded ? "50%" : "0",
               objectFit: "cover",
               objectPosition: "center",
-              flexShrink: 0
+              flexShrink: 0,
             }}
+            onLoad={handleImageLoad}
+            onError={handleImageError}
           />
-          <CardContent sx={{overflow: "auto", scrollbarWidth: "none"}}>
+          <CardContent sx={{ overflow: "auto", scrollbarWidth: "none" }}>
             <Typography gutterBottom variant="h5" component="div">
               {event.name}
             </Typography>
             <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
               {event.date}
             </Typography>
-            <Typography variant="body2" color="text.secondary" >
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{
+                textAlign: "left",
+              }}
+            >
               {event.description}
             </Typography>
           </CardContent>
